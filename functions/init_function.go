c3c20c20fe252dda06f5e6a36d2b001ac1a10944
meherab nahin <nahinrahman87@gmail.com>
2026-01-16 18:22:38 +0600

    init function

---
+++ functions/init_function.go
@@ -0,0 +1,11 @@
+package functions
+
+import "fmt"
+
+func Init() {
+	fmt.Println("I'm the first function that is executed first in the program")
+}
+
+func init() {
+	fmt.Println("I'm  different package's init function that is executed before main functions init ")
+}
---
+++ functions/named_or_standard_fucntion.go
@@ -0,0 +1,7 @@
+package functions
+
+import "fmt"
+
+func NamedOrStandardFunction() {
+	fmt.Println("This is Named or standard function")
+}
